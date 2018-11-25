e17590e64d95dfc26f113a8357c7c4611e4db996
Carlos Eduardo Simon Zepeda <carlos.eduardo@wundertec.com>
2018-11-24 22:18:53 -0600

    node!

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,11 @@ function docker_stop {
     export STOP_PROC=1;
 }
 rm /etc/nginx/conf.d/default.conf
-cat baseServer.conf >> /etc/nginx/conf.d/main.conf
+if [ ! -z "$PROD" ]; then
+    cat baseServer.prod.conf >> /etc/nginx/conf.d/main.conf
+else
+    cat baseServer.dev.conf >> /etc/nginx/conf.d/main.conf
+fi
 
 if  [ ! -z "$ENDPOINTS" ] ; then
   LIST=$(echo $ENDPOINTS | tr ";" "\n")
@@ -17,7 +21,14 @@ if  [ ! -z "$ENDPOINTS" ] ; then
     cat baseLocation.conf | sed -r -e "s~^( *location).*~\1 $LOCATION \{~" -e "s~^( *proxy_pass).*~\1 $PASS;~" >> /etc/nginx/conf.d/main.conf
   done
 fi
-
+if [ ! -z "$PROD" ]; then
+    npm run build &
+else
+    npm start &
+    LOCATION="/"
+    PASS="http://localhost:$PORT"
+    cat baseLocation.conf | sed -r -e "s~^( *location).*~\1 $LOCATION \{~" -e "s~^( *proxy_pass).*~\1 $PASS;~" >> /etc/nginx/conf.d/main.conf
+fi
 echo "}" >> /etc/nginx/conf.d/main.conf
 cat /etc/nginx/conf.d/main.conf
 cd /usr/src/app
