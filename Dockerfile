aecc55b42ecc4bef690edfc5be1c5d43a7166c60
Carlos Simon <casimon@redhat.com>
2020-09-01 13:42:36 -0500

    base image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM nginx:alpine
+FROM registry.redhat.io/ubi8/ubi
+RUN INSTALL_PKGS="nss_wrapper bind-utils gettext hostname nginx nginx-mod-stream nginx-mod-http-perl" && \
+    yum install -y --setopt=tsflags=nodocs $INSTALL_PKGS
 WORKDIR /usr/src/app
 COPY . .
-EXPOSE 80
-ENTRYPOINT [ "/bin/sh","entrypoint.sh" ]
+RUN chmod +x /usr/src/app/entrypoint.sh
+ENTRYPOINT [ "./entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,18 @@ trap "docker_stop" SIGINT SIGTERM
 function docker_stop {
     export STOP_PROC=1;
 }
-rm /etc/nginx/conf.d/default.conf
-cat baseServer.conf >> /etc/nginx/conf.d/main.conf
-
+cat /usr/src/app/baseServer.conf > /etc/nginx/conf.d/main.conf
 if  [ ! -z "$ENDPOINTS" ] ; then
   LIST=$(echo $ENDPOINTS | tr ";" "\n")
   for ENDPOINT in $LIST
   do
     LOCATION="$(echo $ENDPOINT | awk -F '=' '{print $1}')"
     PASS="$(echo $ENDPOINT | awk -F '=' '{print $2}')"
-    cat baseLocation.conf | sed -r -e "s~^( *location).*~\1 $LOCATION \{~" -e "s~^( *proxy_pass).*~\1 $PASS;~" >> /etc/nginx/conf.d/main.conf
+    cat /usr/src/app/baseLocation.conf | sed -r -e "s~^( *location).*~\1 $LOCATION \{~" -e "s~^( *proxy_pass).*~\1 $PASS;~" >> /etc/nginx/conf.d/main.conf
   done
 fi
-
 echo "}" >> /etc/nginx/conf.d/main.conf
 cat /etc/nginx/conf.d/main.conf
-cd /usr/src/app
 nginx -T
 nginx
 ## Debug
